692b5ab85466790ab50b4c7e942412a65d426077
rudypohu <rudypohu@hotmail.fr>
2021-01-15 11:07:22 +0100

    dernière mise à jour

--- model/AuthManager.php
+++ model/AuthManager.php
@@ -5,11 +5,10 @@ namespace Model;
 class AuthManager extends AccessBdd {
 
     // fonction permettant de récupérer le user et le mdp en BDD
-    public function getUser($username) {
+    public function getUser($pseudo) {
         $this->getBDD();
-
         $req = $this->_db->prepare('SELECT * FROM users WHERE pseudo = ?');
-        $req->execute(array($username));
+        $req->execute(array($pseudo));
         $resultat = $req->fetch(\PDO::FETCH_ASSOC);
         $user = new User($resultat);
         return $user;
